dbda2f1fe29374ea17daaee900a06ad70058e2d9
yAmine <yaminee.ca@gmail.com>
2025-08-26 17:44:50 -0400

    feat: Creation of the category section
    - Creation of the file "category-section.tsx" which represent a category section component.
    
    - Insertion of the category component below the nav bar inside the page header
    
    - Creation of the 'style.module.css' which will contains all the custom css i'll use in the project.

--- web/components/layout/header.tsx
+++ web/components/layout/header.tsx
@@ -1,9 +1,30 @@
 import { HeroNavbar } from "../ui/hero-navbar";
+import CategorySection from "../ui/category-section";
+
 
 export function Header() {
+
+  const list_of_links = [
+
+      {link_label : 'Home', link_path : '/' } ,
+      {link_label : 'Sport', link_path : '/' } ,
+      {link_label : 'Scribe', link_path : '/' } ,
+      {link_label : 'Scribe', link_path : '/' } ,
+      {link_label : 'Scribe', link_path : '/' } ,
+      {link_label : 'Scribe', link_path : '/' } ,
+      {link_label : 'Scribe', link_path : '/' } ,
+      {link_label : 'Scribe', link_path : '/' } ,
+      {link_label : 'Scribe', link_path : '/' } ,
+
+
+  ];
+
   return (
     <header>
       <HeroNavbar />
+
+      {/** Yamine's component : To review*/}
+      <CategorySection list_of_links={list_of_links} />
     </header>
   );
 }
